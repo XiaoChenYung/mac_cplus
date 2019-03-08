05b438146059e318a364cfb50e0c3aca8613105a
杨晓晨 <xiaochenyung@gmail.com>
2019-03-08 18:41:23 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+ * Filename: /Users/xiaochenyang/Desktop/c/mac_cplus/main.cpp
+ * Path: /Users/xiaochenyang/Desktop/c/mac_cplus
+ * Created Date: Friday, March 8th 2019, 11:40:52 am
+ * Author: xiaochenyang
+ * 
+ * Copyright (c) 2019 Your Company
+ */
+#include <iostream>
+#include <vector>
+int main(int argc, char const *argv[])
+{
+    std::cout << "哈哈哈" << '\n';
+    return 0;
+}
+
